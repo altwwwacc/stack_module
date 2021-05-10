129054fe286b818fdc6ca62f2c7cdd7fb91a5735
alexandryakin <alexandryakin@gmail.com>
2021-05-10 21:37:33 +0300

    init stack

--- stack.js
+++ stack.js
@@ -1,1 +1,43 @@
 // Your implementation goes here
+function Stack() {
+
+    this.data = [...arguments];
+    this.size = [...arguments].length;
+
+    this.pop = () => {
+        this.size--;
+        if(!this.data.length) {
+            return null;
+        }
+        return this.data.pop();
+    };
+
+    this.push = (arg) => {
+        this.size++;
+        this.data.push(arg);
+    };
+
+    this.peek = () => {
+        return this.data[this.data.length - 1];
+    };
+
+    this.swap = () => {
+        this.data[this.data.length - 2] = this.data[this.data.length - 1];
+        this.data[this.data.length - 1] = this.data[this.data.length - 2];
+    };
+
+    this.isEmpty = () => {
+        return !this.data.length;
+    };
+
+    this.getData = () => {
+        console.log(this.data);
+    }
+
+    // this.name = name;
+    // this.myName = () => {
+    //     console.log(this)
+    // }
+}
+
+module.exports = Stack;
